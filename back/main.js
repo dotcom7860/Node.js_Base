cc6d595c61fdf9279be4de6d492fa540ce0f3769
dotcom7860 <dotcom7860@gmail.com>
2020-10-26 01:55:10 +0900

    함수를 이용하여 정리

--- back/main.js
+++ back/main.js
@@ -3,61 +3,57 @@ var http = require('http');
 var fs = require('fs'); //file System
 var url = require('url'); //require : 요구하다,가져오다
 
+function templateHTML(title, list,body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(files){
+  var list = '<ul>';
+  files.forEach(file => { //forEach : 배열을 가져와서 배열 끝까지 돌림. file = element : 배열 각각의 값
+    list = list + `<li><a href='/?id=${file}'>${file}</a></li>`
+  });
+  list = list +'</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+    var pathname = url.parse(_url, true).pathname; //root디렉토리를 나타내는 값을 가져온다.
 
     if(pathname === '/'){
       if(queryData.id === undefined){
+        fs.readdir('./data',(err,files)=>{
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var list = templateList(files);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);//``:template Literal를 사용하여 값을 넘겨줄 수도 있다.
           response.writeHead(200);
           response.end(template);
+        });
       }
       else {
-        fs.readFile(`./data/${queryData.id}`,'utf8',(err, description) => {
-          var title = queryData.id; //주소의 id값을 가져옴
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+        fs.readdir('./data',(err,files)=>{
+          fs.readFile(`./data/${queryData.id}`,'utf8',(err, description) => {
+            var title = queryData.id; //주소의 id값을 가져옴
+            var list = templateList(files);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
       });
   }
   } else{
