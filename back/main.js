3062b01b1b8d02efd9c995cd010b719b1ecc3615
dotcom7860 <dotcom7860@gmail.com>
2020-10-25 17:55:51 +0900

    오류 구현 및 홈 구현

--- back/main.js
+++ back/main.js
@@ -6,44 +6,64 @@ var url = require('url'); //require : 요구하다,가져오다
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id; //주소의 id값을 가져옴
+    var pathname = url.parse(_url, true).pathname;
 
-    //console.log(request);
-    if(_url == '/'){ //url이 비어있다면 
-       title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-
-    fs.readFile(`./back/data/${queryData.id}`,'utf8',(err, description) => {
-        if(err){
-            console.error(err);return;
-        }
-        var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ul>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ul>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>
-    `;
-    response.end(template);
-    })
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      }
+      else {
+        fs.readFile(`./data/${queryData.id}`,'utf8',(err, description) => {
+          var title = queryData.id; //주소의 id값을 가져옴
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      });
+  }
+  } else{
+    response.writeHead(404);
+    response.end('Not found');
+  }
 });
 
 app.listen(3001);
