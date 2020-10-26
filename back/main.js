475343e8dce64aa502fae9c102d0711d9ecea786
dotcom7860 <dotcom7860@gmail.com>
2020-10-26 21:17:59 +0900

    파일생성과 리다이렉션

--- back/main.js
+++ back/main.js
@@ -2,32 +2,8 @@
 var http = require('http');
 var fs = require('fs'); //file System
 var url = require('url'); //require : 요구하다,가져오다
-
-function templateHTML(title, list,body){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${body}
-  </body>
-  </html>
-  `;
-}
-
-function templateList(files){
-  var list = '<ul>';
-  files.forEach(file => { //forEach : 배열을 가져와서 배열 끝까지 돌림. file = element : 배열 각각의 값
-    list = list + `<li><a href='/?id=${file}'>${file}</a></li>`
-  });
-  list = list +'</ul>';
-  return list;
-}
+var templates = require('./template');
+var qs = require('querystring');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -39,8 +15,8 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data',(err,files)=>{
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateList(files);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);//``:template Literal를 사용하여 값을 넘겨줄 수도 있다.
+          var list = templates.templateList(files);
+          var template = templates.templateHTML(title, list, `<h2>${title}</h2>${description}`);//``:template Literal를 사용하여 값을 넘겨줄 수도 있다.
           response.writeHead(200);
           response.end(template);
         });
@@ -49,14 +25,49 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data',(err,files)=>{
           fs.readFile(`./data/${queryData.id}`,'utf8',(err, description) => {
             var title = queryData.id; //주소의 id값을 가져옴
-            var list = templateList(files);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var list = templates.templateList(files);
+            var template = templates.templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           });
       });
   }
-  } else{
+  }else if(pathname === '/create'){
+    fs.readdir('./data',(err,files)=>{
+      var title = 'WEB - create';
+      var list = templates.templateList(files);
+      var template = templates.templateHTML(title, list, `
+      <form action="http://localhost:3001/create_process" method="POST">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+            <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+      </form>
+      `);//``:template Literal를 사용하여 값을 넘겨줄 수도 있다.
+      response.writeHead(200);
+      response.end(template);
+    });
+  } 
+  else if(pathname === '/create_process'){
+    var body = '';
+    //post방식으로 넘어온 데이터를 받는 방법
+    request.on('data',function(data){//data에 데이터를 하나씩 가져오며 콜백함수에 실행시키다가
+      body = body + data;
+    });
+    request.on('end',function(){//없으면 마지막으로 end로 넘어와서 콜백함수를 실행함
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`./data/${title}`,description,'utf8',(err)=>{
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      });
+    });
+  }
+  else{
     response.writeHead(404);
     response.end('Not found');
   }
---
+++ back/template.js
@@ -0,0 +1,27 @@
+module.exports = {
+    templateHTML:function(title, list,body){
+        return `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <a href="./create">create</a>
+          ${body}
+        </body>
+        </html>
+        `;
+    },
+    templateList:function (files){
+        var list = '<ul>';
+        files.forEach(file => { //forEach : 배열을 가져와서 배열 끝까지 돌림. file = element : 배열 각각의 값
+            list = list + `<li><a href='/?id=${file}'>${file}</a></li>`
+        });
+        list = list +'</ul>';
+        return list;
+    }
+}
